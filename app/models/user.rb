3757b6bd824a6a596061e8af25c8de64ecb718f1
Michelle Lee <mlee092@Michelles-MacBook.local>
2019-10-22 09:20:00 -0400

    Done.

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  devise :omniauthable, :omniauth_providers => [:facebook]
+  
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         def self.from_omniauth(auth)
+          where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+            user.email = auth.info.email
+            user.password = Devise.friendly_token[0,20]
+          end
+        end
+
+end
